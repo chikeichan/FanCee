20ca5f807ed33d0567f69d4741d3120ab1c67504
Chi Kei Chan <chikeichan@gmail.com>
2014-11-12 14:04:32 -0800

    Pong mechanism added

--- module/app.js
+++ module/app.js
@@ -93,9 +93,18 @@ mj.displayCard = function(set, position){
 
 mj.displayOpponents = function(set, position) {
 	var $pos = $('div#'+position);
+	set.sort();
 	for (var i=0; i<set.length; i++){
 		var mj = '<img id="'+position+'" src="../graphics/'+set[i]+'.png"></img>';
 		$pos.append(mj);
+	}
+}
+
+mj.displayPong = function(set, position) {
+	var $pos = $('div#'+position);
+	for (var i=0; i<set.length; i++){
+		var mj = '<img id="p'+position+'" src="../graphics/'+set[i]+'.png"></img>';
+		$pos.prepend(mj);
 	}
 }
 
@@ -137,9 +146,70 @@ mj.ifPong = function(position){
 	return [pong, playerPong];
 }
 
+mj.pongAI = function(position){
+	return true;
+}
+
+mj.PongmySets = [];
+mj.PongrSets = [];
+mj.PonguSets = [];
+mj.PonglSets = [];
+
+mj.pong = function(position){
+	var set;
+	if(position === 'right'){
+			set = 'rSets';
+	} else if (position === 'left'){
+		set = 'lSets';
+	} else if (position === 'up') {
+		set = 'uSets';
+	} else if (position === 'me') {
+		set = 'mySets';
+	}
+
+	var slastSet = mj.playedSets.pop()
+
+	mj[set] = _.without(mj[set],slastSet);
+
+	mj['Pong'+set].push(slastSet);
+	mj['Pong'+set].push(slastSet);
+	mj['Pong'+set].push(slastSet);
+	
+	
+	mj.refresh(position);
+	mj.displayPong(mj['Pong'+set],position);
+	mj.refresh('field');
+
+	_.delay(function(){
+			if(position === 'right'){
+				mj.currentPos === 'right';
+				mj.next('right',false);
+			} else if (position === 'left'){
+				mj.currentPos = 'left';
+				mj.next('left',false);
+			} else if (position === 'up') {
+				mj.currentPos = 'up';
+				mj.next('up',false);
+			} else if (position === 'me') {
+				mj.currentPos = 'me';
+			}
+	},500);
+
+}
+
+
 mj.listenPong = function(position){
 	var lastSet = mj.playedSets[mj.playedSets.length-1];
+
 	
+	if(position!=='me'){
+		if(mj.pongAI(position)){
+			mj.pong(position);
+		}
+	} else {
+		$('div#panel').fadeIn(200);
+
+	}
 }
 
 $(document).ready(function(){
@@ -162,6 +232,20 @@ $(document).ready(function(){
 		},100);
 	});
 
+	$(document).on('mouseenter','div#panel div',function(){
+		$(this).css('background-color','rgba(255, 255, 255, 0.08)');
+	});
+
+	$(document).on('mouseleave','div#panel div',function(){
+		$(this).css('background-color','');
+	});
+
+	$(document).on('click', 'div#Pong', function(){
+		mj.pong('me');
+		$('div#panel').fadeOut(200);
+	})
+
+
 	mj.readSet = function(set){
 		set = set.replace('.png','');
 		set = set.split('/');
@@ -171,8 +255,29 @@ $(document).ready(function(){
 
 	mj.refresh = function(position){
 		var $pos = $('div#'+position);
+		var set;
+		if(position === 'right'){
+			set = 'rSets';
+		} else if (position === 'left'){
+			set = 'lSets';
+		} else if (position === 'up') {
+			set = 'uSets';
+		} else if (position === 'me') {
+			set = 'mySets';
+		} else if (position === 'field'){
+			set = 'field';
+		}
+
 		$pos.html('');
-		mj.displayCard(mj.mySets, position);
+		if(set === 'mySets'){
+			mj.displayCard(mj[set], position);
+		} else if (set === 'field') {
+			_.each(mj.playedSets, function(set){
+				$('div#field').append('<img id="field" src="../graphics/'+set+'.png"></img>');
+			});
+		} else {
+			mj.displayOpponents(mj[set], position);
+		}
 	}
 
 	$(document).on('click','img#me',function(){
@@ -186,20 +291,24 @@ $(document).ready(function(){
 			})
 			mj.mySets.splice(index,1);
 			mj.playedSets.push(setStr);
-			$('div#field').append('<img id="field" src="../graphics/'+setStr+'.png"></img>');
+			mj.refresh('field');
 			mj.refresh('me');
+			mj.displayPong(mj['PongmySets'],'me');
 			mj.currentPos = 'right';
 
-			console.log(mj.ifPong('me'));
-
-			_.delay(function(){
-				mj.next('right');
-			},500);
+			if(mj.ifPong('me')[0]){
+				mj.listenPong(mj.ifPong('me')[1]);
+			} else {
+				_.delay(function(){
+					mj.next('right');
+				},500);
+			}
 		}
 	})
 
-	mj.next = function(position){
+	mj.next = function(position, draw){
 		var set;
+		var $pos = $('div#'+position);
 		if(position === 'right'){
 			set = 'rSets';
 		} else if (position === 'left'){
@@ -210,24 +319,26 @@ $(document).ready(function(){
 			mj.currentPos = 'me';
 		}
 
-		mj[set].push(mj.gameSets.pop());
-		var $pos = $('div#'+position);
-		$pos.html('');
-		mj.displayOpponents(mj[set], position);
+		if(draw === false) {
+
+		} else {
+			mj[set].push(mj.gameSets.pop());
+			mj.refresh(position);
+		}
 
 		var playedSet = mj[set].splice(5,1)[0]
 		mj.playedSets.push(playedSet);
-		$('div#field').append('<img id="field" src="../graphics/'+playedSet+'.png"></img>');
-		$pos.html('');
-		mj.displayOpponents(mj[set], position);
 
+		mj.refresh('field');
+		mj.refresh(position);
+		mj.displayPong(mj['Pong'+set],position);
 
 
 		if(mj.ifPong(position)[0]){
 			mj.listenPong(mj.ifPong(position)[1]);
 		} else {
 			_.delay(function(){
-				if(mj.gameSets.length > 0) {
+				if(mj.gameSets.length > 4) {
 					if(position === 'right'){
 						mj.currentPos === 'up';
 						mj.next('up');
@@ -235,6 +346,7 @@ $(document).ready(function(){
 						mj.currentPos = 'me';
 						mj.mySets.push(mj.gameSets.pop());
 						mj.refresh('me');
+						mj.displayPong(mj['PongmySets'],'me');
 					} else if (position === 'up') {
 						mj.currentPos = 'left';
 						mj.next('left');
