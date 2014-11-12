63cd779d21d9482be7e3fc4c0c4e50c1ca549387
Chi Kei Chan <chikeichan@gmail.com>
2014-11-12 00:18:01 -0800

    Added ability to take turns and draw cards

--- module/app.js
+++ module/app.js
@@ -1,196 +1,195 @@
-var app = angular.module('app', ['ngRoute']);
+var mj = {};
 
-app.config(function($routeProvider){
-	$routeProvider
-		.when('/', {
-			controller: 'ctrl',
-			templateUrl: '../view/mj.html'
-		})
-		.otherwise({redirectTo:'/'});
-});
-
-app.factory('mahjongFactory', function(){
-	var factory = {};
-
-		factory.defaultSets = {
-		bamboo : {
-			'1': 4,
-			'2': 4,
-			'3': 4,
-			'4': 4,
-			'5': 4,
-			'6': 4,
-			'7': 4,
-			'8': 4,
-			'9': 4
-		},
-		man : {
-			'1': 4,
-			'2': 4,
-			'3': 4,
-			'4': 4,
-			'5': 4,
-			'6': 4,
-			'7': 4,
-			'8': 4,
-			'9': 4
-		},
-		pin : {
-			'1': 4,
-			'2': 4,
-			'3': 4,
-			'4': 4,
-			'5': 4,
-			'6': 4,
-			'7': 4,
-			'8': 4,
-			'9': 4
-		},
-		dragon : {
-			'chun': 4,
-			'green': 4,
-			'haku' : 4
-		},
-		wind : {
-			'east' : 4,
-			'north' : 4,
-			'south' : 4,
-			'west' : 4
-		}
+mj.fullSets = {
+	bamboo : {
+		'1': 4,
+		'2': 4,
+		'3': 4,
+		'4': 4,
+		'5': 4,
+		'6': 4,
+		'7': 4,
+		'8': 4,
+		'9': 4
+	},
+	man : {
+		'1': 4,
+		'2': 4,
+		'3': 4,
+		'4': 4,
+		'5': 4,
+		'6': 4,
+		'7': 4,
+		'8': 4,
+		'9': 4
+	},
+	pin : {
+		'1': 4,
+		'2': 4,
+		'3': 4,
+		'4': 4,
+		'5': 4,
+		'6': 4,
+		'7': 4,
+		'8': 4,
+		'9': 4
+	},
+	zdragon : {
+		'chun': 4,
+		'green': 4,
+		'haku' : 4
+	},
+	wind : {
+		'east' : 4,
+		'north' : 4,
+		'south' : 4,
+		'west' : 4
 	}
+}
 
-	factory.fullSets = {
-		bamboo : {
-			'1': 4,
-			'2': 4,
-			'3': 4,
-			'4': 4,
-			'5': 4,
-			'6': 4,
-			'7': 4,
-			'8': 4,
-			'9': 4
-		},
-		man : {
-			'1': 4,
-			'2': 4,
-			'3': 4,
-			'4': 4,
-			'5': 4,
-			'6': 4,
-			'7': 4,
-			'8': 4,
-			'9': 4
-		},
-		pin : {
-			'1': 4,
-			'2': 4,
-			'3': 4,
-			'4': 4,
-			'5': 4,
-			'6': 4,
-			'7': 4,
-			'8': 4,
-			'9': 4
-		},
-		dragon : {
-			'chun': 4,
-			'green': 4,
-			'haku' : 4
-		},
-		wind : {
-			'east' : 4,
-			'north' : 4,
-			'south' : 4,
-			'west' : 4
-		}
-	}
-
-	factory.shuffleSets = function(){
-		var sets = [];
-			_.each(factory.fullSets, function(x,type){
-				_.each(x, function(num,set){
-					for(var i =0; i<num; i++){
-						sets.push(type+'-'+set);
-						factory.fullSets[type][set]--;
-					}
-				})
+mj.shuffleSets = function(){
+	var sets = [];
+		_.each(mj.fullSets, function(x,type){
+			_.each(x, function(num,set){
+				for(var i =0; i<num; i++){
+					sets.push(type+'-'+set);
+					mj.fullSets[type][set]--;
+				}
 			})
-		return _.shuffle(sets);
+		})
+	return _.shuffle(sets);
+}
+
+mj.mySets = [];
+mj.rSets = [];
+mj.uSets = [];
+mj.lSets = [];
+mj.playedSets = [];
+mj.currentPos = 'me';
+
+
+
+mj.readySets = function(sets){
+	while(this.gameSets.length > 83){
+		this.mySets.push(this.gameSets.pop());
+		if(this.mySets.length>13) {return};
+		this.rSets.push(this.gameSets.pop());
+		if(this.rSets.length>13) {return};
+		this.uSets.push(this.gameSets.pop());
+		if(this.uSets.length>13) {return};
+		this.lSets.push(this.gameSets.pop());
+		if(this.lSets.length>13) {return};
 	}
-	factory.mySets = [];
-	factory.rSets = [];
-	factory.uSets = [];
-	factory.lSets = [];
+}
 
-	//factory.gameSets = [];
-
-
-	factory.readySets = function(sets){
-		while(this.gameSets.length > 83){
-			this.mySets.push(this.gameSets.pop());
-			if(this.mySets.length>13) {return};
-			this.rSets.push(this.gameSets.pop());
-			if(this.rSets.length>13) {return};
-			this.uSets.push(this.gameSets.pop());
-			if(this.uSets.length>13) {return};
-			this.lSets.push(this.gameSets.pop());
-			if(this.lSets.length>13) {return};
-		}
+mj.displayCard = function(set, position){
+	var $pos = $('div#'+position);
+	set.sort();
+	for (var i=0; i<set.length; i++){
+		var mj = '<img id="'+position+'" src="../graphics/'+set[i]+'.png"></img>';
+		$pos.append(mj);
 	}
+}
 
-	factory.displayCard = function(set, position){
-		var $pos = $('div#'+position);
-		set.sort();
-		console.log(set.length);
-		for (var i=0; i<set.length; i++){
-			var mj = '<img id="'+position+'" src="../graphics/'+set[i]+'.png"></img>';
-			$pos.append(mj);
-		}
+mj.displayOpponents = function(set, position) {
+	var $pos = $('div#'+position);
+	for (var i=0; i<set.length; i++){
+		var mj = '<img id="'+position+'" src="../graphics/'+set[i]+'.png"></img>';
+		$pos.append(mj);
 	}
+}
 
-	factory.displayOpponents = function(set, position) {
-		var $pos = $('div#'+position);
-		console.log(set.length);
-		for (var i=0; i<set.length; i++){
-			var mj = '<img id="'+position+'" src="../graphics/face-down-128px.png"></img>';
-			$pos.append(mj);
-			$('img').fadeIn('slow');
-		}
-	}
+$(document).ready(function(){
+	mj.gameSets = mj.shuffleSets();
+	mj.readySets();
+	mj.displayCard(mj.mySets, 'me');
+	mj.displayOpponents(mj.rSets, 'right');
+	mj.displayOpponents(mj.uSets, 'up');
+	mj.displayOpponents(mj.lSets, 'left');
 
-	return factory;
-});
-
-app.controller('ctrl', ['$scope','$http', 'mahjongFactory',function ctrl($scope, $http, mahjongFactory){
-	$scope.mySets = [];
-	$scope.rSets = [];
-	$scope.lSets = [];
-	$scope.uSets = [];
-	$scope.readySets = mahjongFactory.readySets;
-	$scope.displayCard = mahjongFactory.displayCard;
-	$scope.displayOpponents = mahjongFactory.displayOpponents;
-	$scope.shuffleSets = mahjongFactory.shuffleSets;
-
-	$scope.gameSets = mahjongFactory.shuffleSets();
-	$scope.readySets();
-	$scope.displayCard($scope.mySets, 'me');
-	$scope.displayOpponents($scope.rSets, 'right');
-	$scope.displayOpponents($scope.uSets, 'up');
-	$scope.displayOpponents($scope.lSets, 'left');
-	console.log(mahjongFactory.shuffleSets());
-
-	$('img#me').mouseenter(function(){
+	$(document).on('mouseenter','img#me',function(){
 		$(this).stop().animate({
 			'top': '-20px'
 		},100);
-	})
+	});
 
-	$('img#me').mouseleave(function(){
+	$(document).on('mouseleave','img#me',function(){
 		$(this).stop().animate({
 			'top': '0px'
 		},100);
+	});
+
+	mj.readSet = function(set){
+		set = set.replace('.png','');
+		set = set.split('/');
+		set = set[set.length-1];
+		return set.split('-');
+	};
+
+	mj.refresh = function(position){
+		var $pos = $('div#'+position);
+		$pos.html('');
+		mj.displayCard(mj.mySets, position);
+	}
+
+	$(document).on('click','img#me',function(){
+		if(mj.currentPos === 'me') {
+			var set = mj.readSet(this.src);
+			var setStr = set[0]+'-'+set[1];
+			var index = 0;
+			_.find(mj.mySets, function(x,i){
+					index = i;
+					return x === setStr;
+			})
+			mj.mySets.splice(index,1);
+			mj.playedSets.push(setStr);
+			$('div#field').append('<img id="field" src="../graphics/'+setStr+'.png"></img>');
+			mj.refresh('me');
+			mj.currentPos = 'right';
+			mj.next('right');
+		}
 	})
 
+	mj.next = function(position){
+		var set;
+		if(position === 'right'){
+			set = 'rSets';
+		} else if (position === 'left'){
+			set = 'lSets';
+		} else if (position === 'up') {
+			set = 'uSets';
+		} else if (position === 'me') {
+			mj.currentPos = 'me';
+		}
 
-}]);
+		mj[set].push(mj.gameSets.pop());
+		var $pos = $('div#'+position);
+		$pos.html('');
+		mj.displayOpponents(mj[set], position);
+
+		var playedSet = mj[set].splice(5,1)
+		mj.playedSets.push(playedSet);
+		$('div#field').append('<img id="field" src="../graphics/'+playedSet+'.png"></img>');
+		$pos.html('');
+		mj.displayOpponents(mj[set], position);
+		
+		if(mj.gameSets.length > 0) {
+			if(position === 'right'){
+				mj.currentPos === 'up';
+				mj.next('up');
+			} else if (position === 'left'){
+				mj.currentPos = 'me';
+				mj.mySets.push(mj.gameSets.pop());
+				mj.refresh('me');
+			} else if (position === 'up') {
+				mj.currentPos = 'left';
+				mj.next('left');
+			}
+		} 
+		
+
+	}
+
+
+
+})
