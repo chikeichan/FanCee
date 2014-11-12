9cba687ddde4b766f1bff8d367595db08e36830b
Chi Kei Chan <chikeichan@gmail.com>
2014-11-12 14:25:24 -0800

    Added comments and re-organized codes

--- module/app.js
+++ module/app.js
@@ -1,5 +1,8 @@
 var mj = {};
 
+//Initialize Sets ==============================================
+
+//Original Full Sets
 mj.fullSets = {
 	bamboo : {
 		'1': 4,
@@ -47,6 +50,7 @@ mj.fullSets = {
 	}
 }
 
+//Function to return a shuffled array of MahJong sets
 mj.shuffleSets = function(){
 	var sets = [];
 		_.each(mj.fullSets, function(x,type){
@@ -60,15 +64,21 @@ mj.shuffleSets = function(){
 	return _.shuffle(sets);
 }
 
+//Global variables Declaration;
 mj.mySets = [];
 mj.rSets = [];
 mj.uSets = [];
 mj.lSets = [];
 mj.playedSets = [];
 mj.currentPos = 'me';
+mj.PongmySets = [];
+mj.PongrSets = [];
+mj.PonguSets = [];
+mj.PonglSets = [];
 
 
-
+//Starting at player's position, each position takes turn getting a card counter clockwise
+//until player has 14 card.
 mj.readySets = function(sets){
 	while(this.gameSets.length > 83){
 		this.mySets.push(this.gameSets.pop());
@@ -82,6 +92,7 @@ mj.readySets = function(sets){
 	}
 }
 
+//Display sets face up for one player
 mj.displayCard = function(set, position){
 	var $pos = $('div#'+position);
 	set.sort();
@@ -91,6 +102,7 @@ mj.displayCard = function(set, position){
 	}
 }
 
+//Display sets face down for opponents
 mj.displayOpponents = function(set, position) {
 	var $pos = $('div#'+position);
 	set.sort();
@@ -100,6 +112,7 @@ mj.displayOpponents = function(set, position) {
 	}
 }
 
+//Display Ponged sets face up for everyone
 mj.displayPong = function(set, position) {
 	var $pos = $('div#'+position);
 	for (var i=0; i<set.length; i++){
@@ -108,7 +121,8 @@ mj.displayPong = function(set, position) {
 	}
 }
 
-mj.ifPong = function(position){
+//Analyzed recently played set to see if a pause is needed.
+mj.ifPause = function(position){
 	var pong = false;
 	var playerPong;
 	var lastSet = mj.playedSets[mj.playedSets.length-1];
@@ -146,15 +160,14 @@ mj.ifPong = function(position){
 	return [pong, playerPong];
 }
 
+
+//AI to return boolean whether or not to pong.
 mj.pongAI = function(position){
 	return true;
 }
 
-mj.PongmySets = [];
-mj.PongrSets = [];
-mj.PonguSets = [];
-mj.PonglSets = [];
 
+//Execute Pong sequence
 mj.pong = function(position){
 	var set;
 	if(position === 'right'){
@@ -198,20 +211,112 @@ mj.pong = function(position){
 }
 
 
+//When a pause is caused by Pong scenario, listen for Pong executive command.
 mj.listenPong = function(position){
 	var lastSet = mj.playedSets[mj.playedSets.length-1];
 
-	
 	if(position!=='me'){
 		if(mj.pongAI(position)){
 			mj.pong(position);
 		}
 	} else {
 		$('div#panel').fadeIn(200);
-
 	}
 }
 
+//Return set string to [set Type, set Number]
+mj.readSet = function(set){
+	set = set.replace('.png','');
+	set = set.split('/');
+	set = set[set.length-1];
+	return set.split('-');
+};
+
+
+//Refresh rendering for me, right, up, left, or field.
+mj.refresh = function(position){
+	var $pos = $('div#'+position);
+	var set;
+	if(position === 'right'){
+		set = 'rSets';
+	} else if (position === 'left'){
+		set = 'lSets';
+	} else if (position === 'up') {
+		set = 'uSets';
+	} else if (position === 'me') {
+		set = 'mySets';
+	} else if (position === 'field'){
+		set = 'field';
+	}
+
+	$pos.html('');
+	if(set === 'mySets'){
+		mj.displayCard(mj[set], position);
+	} else if (set === 'field') {
+		_.each(mj.playedSets, function(set){
+			$('div#field').append('<img id="field" src="../graphics/'+set+'.png"></img>');
+		});
+	} else {
+		mj.displayOpponents(mj[set], position);
+	}
+}
+
+//Main recursive logic to execute next round of playing until there are only 4
+//cards left in the gamesets
+mj.next = function(position, draw){
+	var set;
+	var $pos = $('div#'+position);
+	if(position === 'right'){
+		set = 'rSets';
+	} else if (position === 'left'){
+		set = 'lSets';
+	} else if (position === 'up') {
+		set = 'uSets';
+	} else if (position === 'me') {
+		mj.currentPos = 'me';
+	}
+
+	if(draw === false) {
+
+	} else {
+		mj[set].push(mj.gameSets.pop());
+		mj.refresh(position);
+	}
+
+	var playedSet = mj[set].splice(5,1)[0]
+	mj.playedSets.push(playedSet);
+
+	mj.refresh('field');
+	mj.refresh(position);
+	mj.displayPong(mj['Pong'+set],position);
+
+
+	if(mj.ifPause(position)[0]){
+		mj.listenPong(mj.ifPause(position)[1]);
+	} else {
+		_.delay(function(){
+			if(mj.gameSets.length > 4) {
+				if(position === 'right'){
+					mj.currentPos === 'up';
+					mj.next('up');
+				} else if (position === 'left'){
+					mj.currentPos = 'me';
+					mj.mySets.push(mj.gameSets.pop());
+					mj.refresh('me');
+					mj.displayPong(mj['PongmySets'],'me');
+				} else if (position === 'up') {
+					mj.currentPos = 'left';
+					mj.next('left');
+				}
+			}
+		},500);
+	}
+}
+
+
+
+
+//DOM Manipulation and Interactions
 $(document).ready(function(){
 	mj.gameSets = mj.shuffleSets();
 	mj.readySets();
@@ -245,41 +350,6 @@ $(document).ready(function(){
 		$('div#panel').fadeOut(200);
 	})
 
-
-	mj.readSet = function(set){
-		set = set.replace('.png','');
-		set = set.split('/');
-		set = set[set.length-1];
-		return set.split('-');
-	};
-
-	mj.refresh = function(position){
-		var $pos = $('div#'+position);
-		var set;
-		if(position === 'right'){
-			set = 'rSets';
-		} else if (position === 'left'){
-			set = 'lSets';
-		} else if (position === 'up') {
-			set = 'uSets';
-		} else if (position === 'me') {
-			set = 'mySets';
-		} else if (position === 'field'){
-			set = 'field';
-		}
-
-		$pos.html('');
-		if(set === 'mySets'){
-			mj.displayCard(mj[set], position);
-		} else if (set === 'field') {
-			_.each(mj.playedSets, function(set){
-				$('div#field').append('<img id="field" src="../graphics/'+set+'.png"></img>');
-			});
-		} else {
-			mj.displayOpponents(mj[set], position);
-		}
-	}
-
 	$(document).on('click','img#me',function(){
 		if(mj.currentPos === 'me') {
 			var set = mj.readSet(this.src);
@@ -296,8 +366,8 @@ $(document).ready(function(){
 			mj.displayPong(mj['PongmySets'],'me');
 			mj.currentPos = 'right';
 
-			if(mj.ifPong('me')[0]){
-				mj.listenPong(mj.ifPong('me')[1]);
+			if(mj.ifPause('me')[0]){
+				mj.listenPong(mj.ifPause('me')[1]);
 			} else {
 				_.delay(function(){
 					mj.next('right');
@@ -306,57 +376,7 @@ $(document).ready(function(){
 		}
 	})
 
-	mj.next = function(position, draw){
-		var set;
-		var $pos = $('div#'+position);
-		if(position === 'right'){
-			set = 'rSets';
-		} else if (position === 'left'){
-			set = 'lSets';
-		} else if (position === 'up') {
-			set = 'uSets';
-		} else if (position === 'me') {
-			mj.currentPos = 'me';
-		}
 
-		if(draw === false) {
-
-		} else {
-			mj[set].push(mj.gameSets.pop());
-			mj.refresh(position);
-		}
-
-		var playedSet = mj[set].splice(5,1)[0]
-		mj.playedSets.push(playedSet);
-
-		mj.refresh('field');
-		mj.refresh(position);
-		mj.displayPong(mj['Pong'+set],position);
-
-
-		if(mj.ifPong(position)[0]){
-			mj.listenPong(mj.ifPong(position)[1]);
-		} else {
-			_.delay(function(){
-				if(mj.gameSets.length > 4) {
-					if(position === 'right'){
-						mj.currentPos === 'up';
-						mj.next('up');
-					} else if (position === 'left'){
-						mj.currentPos = 'me';
-						mj.mySets.push(mj.gameSets.pop());
-						mj.refresh('me');
-						mj.displayPong(mj['PongmySets'],'me');
-					} else if (position === 'up') {
-						mj.currentPos = 'left';
-						mj.next('left');
-					}
-				}
-			},500);
-		}
-		
-
-	}
 
 
 
